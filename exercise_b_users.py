9506908c47c612a896c0ebfe327423328c3fce41
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-12 16:29:19 +0000

    completed task 6

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -83,8 +83,19 @@ for number in users["Avril"]["lottery_numbers"]:
 print(avril_even_lottery)
 
 
-
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"]["lottery_numbers"].append(7)
+print(users["Erik"]["lottery_numbers"])
+
+
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["home_town"] = "Edinburgh"
+print(users["Erik"]["home_town"])
+
+
 # 9. Add a pet dog to Erik called "Fluffy"
+users["Erik"]["pets"].append({"name": "Fluffy", "species": "dog"})
+print(users["Erik"]["pets"])
+
+
 # 10. Add another person to the users dictionary
